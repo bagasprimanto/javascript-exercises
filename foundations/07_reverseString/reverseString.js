4a9bd5a7d3e7db37c21b34089e94aa38a9c8c4f0
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-15 09:40:13 +0700

    Finished 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    let reversedStr = '';
 
+    for (let i = 1; i <= str.length; i++) {
+        reversedStr += str.at(str.length - i);
+    }
+
+    return reversedStr;
 };
 
 // Do not edit below this line
