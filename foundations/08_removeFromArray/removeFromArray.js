73308d3952fe251c5b0e236666e5a735874aa398
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-15 10:11:35 +0700

    Finished 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+    const valuesToRemove = Array.prototype.slice.call(arguments, 1);
+
+    for (const value of valuesToRemove) {
+        while (arr.includes(value)) {
+            let deleteIndex = arr.findIndex(elem => elem === value);
+            arr.splice(deleteIndex, 1);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
