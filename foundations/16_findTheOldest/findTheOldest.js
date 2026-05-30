edbc66dcdd3b73ad1d367424c5f7f74c36929d88
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-30 16:57:15 +0700

    Finished 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrPerson) {
+    return arrPerson.sort((personA, personB) => {
+        let ageA;
+        let ageB;
 
+        if (!personA.yearOfDeath) {
+            let currYear = new Date().getFullYear();
+            ageA = currYear - personA.yearOfBirth;
+        } else {
+            ageA = personA.yearOfDeath - personA.yearOfBirth;
+        }
+
+        if (!personB.yearOfDeath) {
+            let currYear = new Date().getFullYear();
+            ageB = currYear - personB.yearOfBirth;
+        } else {
+            ageB = personB.yearOfDeath - personB.yearOfBirth;
+        }
+
+        return -1 * (ageA - ageB);
+    })[0];
 };
 
 // Do not edit below this line
