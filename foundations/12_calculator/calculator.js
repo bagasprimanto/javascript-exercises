c0a16eed75e1526e09239344316318f1b55eeac0
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-30 15:58:43 +0700

    Finished 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  let totalSum = arr.reduce((tempSum, elem) => {
+    tempSum += elem;
+    return tempSum;
+  }, 0);
+
+  return totalSum;
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let totalMultiply = arr.reduce((tempMult, elem) => {
+    tempMult *= elem;
+    return tempMult;
+  });
 
+  return totalMultiply;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
