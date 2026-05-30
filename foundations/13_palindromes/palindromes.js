bb0dee8801034e6376093c941dc68bde42d1e126
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-30 16:15:51 +0700

    Finished 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strArr = str.toLowerCase().replace(/[^A-Z0-9]+/ig, "").split("");
+    // console.log(strArr);
+    let strArrCopy = strArr.slice();
 
+    strArrCopy.reverse();
+    // console.log(strArrCopy);
+
+    for (let i = 0; i < strArr.length; i++) {
+        if (strArr.at(i) !== strArrCopy.at(i)) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
